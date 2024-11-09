32eb0f0053d793021eb17cb1509de825d4a1a57b
Anand Balaram <anandbalaram@cmail.carleton.ca>
2024-11-09 15:16:07 -0500

    Renamed SurveyorRepository.java to UserRepository.java

--- src/main/java/sysc4806/project24/mini_survey_monkey/repositories/UserRepository.java
+++ src/main/java/sysc4806/project24/mini_survey_monkey/repositories/UserRepository.java
@@ -3,4 +3,4 @@ package sysc4806.project24.mini_survey_monkey.repositories;
 import org.springframework.data.repository.CrudRepository;
 import sysc4806.project24.mini_survey_monkey.models.User;
 
-public interface SurveyorRepository extends CrudRepository<User, Integer> {}
+public interface UserRepository extends CrudRepository<User, Integer> {}
