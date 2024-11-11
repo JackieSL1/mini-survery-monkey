3c722afe48f83c262a0852e374f2f20cdc1c4bff
Braeden Kloke <braedenkloke@gmail.com>
2024-11-11 09:51:15 -0500

    remove extra user repository
    This one snuck by us ...

--- src/main/java/sysc4806/project24/mini_survey_monkey/repositories/UserRespository.java
+++
@@ -1,6 +0,0 @@
-package sysc4806.project24.mini_survey_monkey.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import sysc4806.project24.mini_survey_monkey.models.User;
-
-public interface UserRespository extends CrudRepository<User, Integer> {}
