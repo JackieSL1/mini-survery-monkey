bd29d4da48a86263de6c0b5f8095f9a5291cc67a
Braeden Kloke <braedenkloke@gmail.com>
2024-11-11 07:51:10 -0500

    refactor: moved integration tests to seperate package
    Didn't make sense to keep integration tests with model tests.

--- src/test/java/sysc4806/project24/mini_survey_monkey/integration/ControllerIntegrationTest.java
+++ src/test/java/sysc4806/project24/mini_survey_monkey/integration/ControllerIntegrationTest.java
@@ -1,10 +1,11 @@
-package sysc4806.project24.mini_survey_monkey.models;
+package sysc4806.project24.mini_survey_monkey.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import sysc4806.project24.mini_survey_monkey.models.State;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
