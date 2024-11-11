0e6d6a8a5d14bf050a3bb5a751dc7bf7c713393c
Braeden Kloke <braedenkloke@gmail.com>
2024-11-11 08:26:24 -0500

    remove: deprecated surveyor model and repository

--- src/main/java/sysc4806/project24/mini_survey_monkey/repositories/SurveyorRepository.java
+++
@@ -1,7 +0,0 @@
-package sysc4806.project24.mini_survey_monkey.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import sysc4806.project24.mini_survey_monkey.models.CommentQuestion;
-import sysc4806.project24.mini_survey_monkey.models.Surveyor;
-
-public interface SurveyorRepository extends CrudRepository<Surveyor, Integer> {}
