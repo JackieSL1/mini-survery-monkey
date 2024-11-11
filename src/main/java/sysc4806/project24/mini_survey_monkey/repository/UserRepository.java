da41a0a3d29f700403d7f45d55c3a779bd00c282
AliAbdollahian <ALIABDOLLAHIAN@cmail.carleton.ca>
2024-11-10 20:36:05 -0500

    Added the following features: - Adding a question text box - Editing the question - deleting the question

--- src/main/java/sysc4806/project24/mini_survey_monkey/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package sysc4806.project24.mini_survey_monkey.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import sysc4806.project24.mini_survey_monkey.model.UserMonkey;
-
-public interface UserRepository extends CrudRepository<UserMonkey,Long> {
-}
