df338a7d0b87fcafc0ca0523cc1e23dbdbd30b6d
Jackie Smolkin-Lerner <81377350+JackieSL1@users.noreply.github.com>
2024-11-15 11:26:02 -0500

    Filter non draft surveys (#69)
    * Add interceptor to filter non-draft surveys from create endpoints
    
    * Add state to filter as argument for reuse
    
    * Fix inverted logic
    
    * Refactor for readability and add comments

---
+++ src/main/java/sysc4806/project24/mini_survey_monkey/WebMvcConfig.java
@@ -0,0 +1,30 @@
+package sysc4806.project24.mini_survey_monkey;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import sysc4806.project24.mini_survey_monkey.interceptors.SurveyStateInterceptor;
+import sysc4806.project24.mini_survey_monkey.models.State;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final SurveyStateInterceptor draftSurveyInterceptor;
+    private final SurveyStateInterceptor nonDraftSurveyInterceptor;
+
+    public WebMvcConfig(SurveyStateInterceptor draftSurveyInterceptor, SurveyStateInterceptor nonDraftSurveyInterceptor) {
+        this.draftSurveyInterceptor = draftSurveyInterceptor;
+        draftSurveyInterceptor.setStatesToFilterOut(List.of(State.DRAFT));
+
+        nonDraftSurveyInterceptor.setStatesToFilterOut(List.of(State.OPEN, State.CLOSED));
+        this.nonDraftSurveyInterceptor = nonDraftSurveyInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(nonDraftSurveyInterceptor).addPathPatterns("/**/create/**");
+//        registry.addInterceptor(draftSurveyInterceptor).addPathPatterns("/**/survey/**"); TODO: Uncomment when survey endpoints are added
+    }
+}
