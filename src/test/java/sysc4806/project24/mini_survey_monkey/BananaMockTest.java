71efd52a703fb5b4d04ff94e7dd74cd0a085b04a
JackieSL1 <jackie@smallcity.ca>
2024-11-15 20:48:32 -0500

    Fix BananaMockTest

--- src/test/java/sysc4806/project24/mini_survey_monkey/BananaMockTest.java
+++ src/test/java/sysc4806/project24/mini_survey_monkey/BananaMockTest.java
@@ -7,14 +7,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import sysc4806.project24.mini_survey_monkey.controllers.BananaController;
+import sysc4806.project24.mini_survey_monkey.interceptors.SurveyStateInterceptor;
+import sysc4806.project24.mini_survey_monkey.repositories.SurveyRepository;
 
 @WebMvcTest(BananaController.class)
 public class BananaMockTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    // This prevents SurveyStateInterceptor from being loaded
+    @MockBean
+    private SurveyStateInterceptor surveyStateInterceptor;
 
     @Test
     public void shouldReturnBanana() throws Exception {
