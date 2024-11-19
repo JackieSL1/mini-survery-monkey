1db57241e423094f3eddf165ce2385bcfc2dd10b
Braeden Kloke <braedenkloke@gmail.com>
2024-11-19 14:23:47 -0500

    init login page

---
+++ src/main/java/sysc4806/project24/mini_survey_monkey/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package sysc4806.project24.mini_survey_monkey.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+}
