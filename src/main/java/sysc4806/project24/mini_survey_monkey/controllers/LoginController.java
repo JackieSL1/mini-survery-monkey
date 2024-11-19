4b7c3af8b390d052d08263d03d3575a9371ec425
Braeden Kloke <braedenkloke@gmail.com>
2024-11-19 14:44:03 -0500

    redirect to home page after entering an email address
    Copies SurveyMonkey login page design.

--- src/main/java/sysc4806/project24/mini_survey_monkey/controllers/LoginController.java
+++ src/main/java/sysc4806/project24/mini_survey_monkey/controllers/LoginController.java
@@ -3,12 +3,18 @@ package sysc4806.project24.mini_survey_monkey.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(Model model) {
+    public String init(Model model) {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(Model model) {
+        return "redirect:/home";
     }
 }
