a0e1b78b151824f2d284678b89e08d2b8476b3ed
Anand Balaram <anandbalaram@cmail.carleton.ca>
2024-11-22 11:59:18 -0500

    Fix test

--- src/test/java/sysc4806/project24/mini_survey_monkey/integration/ControllerIntegrationTest.java
+++ src/test/java/sysc4806/project24/mini_survey_monkey/integration/ControllerIntegrationTest.java
@@ -14,6 +14,7 @@ import sysc4806.project24.mini_survey_monkey.models.State;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -202,21 +203,22 @@ public class ControllerIntegrationTest {
 
     @Test
     public void testNavigatingToSharingLink() throws Exception {
-        int initialSurveyId = 1;
-        mockMvc.perform(post("/create"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(header().string("Location", startsWith("/create/")));
-
-        mockMvc.perform(post("/create/" + initialSurveyId + "/open"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(header().string("Location", "/collect/" + initialSurveyId));
-        mockMvc.perform(get("/collect/" + initialSurveyId))
-                .andExpect(status().isOk())
-                .andExpect(view().name("collect"))
-                .andExpect(content().string(containsString("Copy this URL: ")));
-        mockMvc.perform(get("/r/1"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("fill-in"));
+        mockMvc.perform(post("/create")).andDo(
+                result -> {
+                    String location = result.getResponse().getHeader("Location");
+                    int surveyId = Integer.parseInt(location.split("/")[2]);
+                    mockMvc.perform(post("/create/" + surveyId + "/open"))
+                            .andExpect(status().is3xxRedirection())
+                            .andExpect(header().string("Location", "/collect/" + surveyId));
+                    mockMvc.perform(get("/collect/" + surveyId))
+                            .andExpect(status().isOk())
+                            .andExpect(view().name("collect"))
+                            .andExpect(content().string(containsString("Copy this URL: ")));
+                    mockMvc.perform(get("/r/1"))
+                            .andExpect(status().isOk())
+                            .andExpect(view().name("fill-in"));
+                }
+                );
     }
 }
 
