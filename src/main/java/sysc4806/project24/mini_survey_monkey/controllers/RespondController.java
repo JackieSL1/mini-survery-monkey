93dbb2a81ba34dbac9f451bc4ecbd9b038ab2d81
Anand Balaram <126517682+ajbalara@users.noreply.github.com>
2024-11-23 11:51:10 -0500

    Unique sharing link for each survey (#87)
    * Implemented unique links using surveyID
    
    * Display survey title on respond.html
    
    * Add test to make sure that response page can only be accessed for open survey
    
    ---------
    
    Co-authored-by: Anand Balaram <anandbalaram@cmail.carleton.ca>

--- src/main/java/sysc4806/project24/mini_survey_monkey/controllers/RespondController.java
+++ src/main/java/sysc4806/project24/mini_survey_monkey/controllers/RespondController.java
@@ -1,19 +1,26 @@
 package sysc4806.project24.mini_survey_monkey.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import sysc4806.project24.mini_survey_monkey.models.Survey;
 import sysc4806.project24.mini_survey_monkey.repositories.SurveyRepository;
 
 @Controller
 public class RespondController {
+
     private final SurveyRepository surveyRepository;
 
     public RespondController(SurveyRepository surveyRepository) {
         this.surveyRepository = surveyRepository;
     }
 
-    @GetMapping("/r/1")
-    public String respond() {
+    @GetMapping("/r/{surveyID}")
+    public String respond(@PathVariable("surveyID") int surveyID, Model model) {
+        Survey survey = surveyRepository.findById(surveyID);
+        model.addAttribute("survey", survey);
 
         return "respond";
     }
