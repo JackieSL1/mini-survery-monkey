d81eea49d765c8f7e67bd9453a6d6d858786cf89
JackieSL1 <jackie@smallcity.ca>
2024-11-24 13:09:54 -0500

    Add survey responding

--- src/main/java/sysc4806/project24/mini_survey_monkey/controllers/RespondController.java
+++ src/main/java/sysc4806/project24/mini_survey_monkey/controllers/RespondController.java
@@ -1,16 +1,21 @@
 package sysc4806.project24.mini_survey_monkey.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import sysc4806.project24.mini_survey_monkey.ResponseFormData;
+import sysc4806.project24.mini_survey_monkey.models.CommentResponse;
+import sysc4806.project24.mini_survey_monkey.models.Question;
+import sysc4806.project24.mini_survey_monkey.models.Response;
 import sysc4806.project24.mini_survey_monkey.models.Survey;
+import sysc4806.project24.mini_survey_monkey.repositories.CommentResponseRepository;
 import sysc4806.project24.mini_survey_monkey.repositories.SurveyRepository;
 
 @Controller
 public class RespondController {
-
     private final SurveyRepository surveyRepository;
 
     public RespondController(SurveyRepository surveyRepository) {
@@ -21,7 +26,27 @@ public class RespondController {
     public String respond(@PathVariable("surveyID") int surveyID, Model model) {
         Survey survey = surveyRepository.findById(surveyID);
         model.addAttribute("survey", survey);
+        // TODO: Handle form having no questions
+        ResponseFormData formData = new ResponseFormData();
+        formData.setQuestion(survey.getQuestions().get(0));
+        model.addAttribute("formData", formData);
 
         return "respond";
+    }
+
+    @PostMapping("/r/{surveyID}/submit")
+    public String submit(@PathVariable("surveyID") int surveyID, @ModelAttribute ResponseFormData formData) {
+        Survey survey = surveyRepository.findById(surveyID);
+        CommentResponse response = new CommentResponse();
+        response.setText(formData.getResponseText());
+
+        Question question = survey.getQuestions().get(0);
+        question.getResponses().add(response);
+        response.setQuestion(question);
+
+        System.out.println("Submitted response: " + response);
+        surveyRepository.save(survey);
+
+        return "redirect:/r/" + surveyID;
     }
 }
---
+++ src/test/java/sysc4806/project24/mini_survey_monkey/integration/ResponseTest.java
@@ -0,0 +1,56 @@
+package sysc4806.project24.mini_survey_monkey.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ResponseTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Tests responding to a survey
+     */
+    @Test
+    public void testResponseSubmission() throws Exception {
+        // Create a survey
+        mockMvc.perform(post("/create")).andDo(result -> {
+            String location = result.getResponse().getHeader("Location");
+            int surveyId = Integer.parseInt(location.split("/")[2]);
+
+            mockMvc.perform(get("/r/" + surveyId)).andExpect(status().is4xxClientError());
+
+            mockMvc.perform(post("/create/" + surveyId + "/update").param("title", "New Survey Title"));
+
+            // /r/1 should throw a 404 while the survey is a Draft
+            mockMvc.perform(get("/r/" + surveyId)).andExpect(status().is4xxClientError());
+
+            mockMvc.perform(post("/create/" + surveyId + "/question"));
+            mockMvc.perform(post("/create/" + surveyId + "/question/1/update").param("newQuestionText",
+                    "This is question 1"));
+            mockMvc.perform(post("/create/" + surveyId + "/open"));
+
+            // /r/1 should return 200 now that the survey is Open
+            mockMvc.perform(get("/r/" + surveyId)).andExpect(status().isOk()).andExpect(model().attribute("survey",
+                    hasProperty("title", equalTo("New Survey Title")))).andExpect(model().attribute("survey",
+                    hasProperty("questions", hasSize(1))));
+
+            mockMvc.perform(post("/r/" + surveyId + "/submit").param("text", "This is my response")).andExpect(status().is3xxRedirection());
+
+            // /r/1 should throw a 404 when the survey is Closed
+            mockMvc.perform(post("/summary/" + surveyId + "/close"));
+            mockMvc.perform(get("/r/" + surveyId)).andExpect(status().is4xxClientError());
+        });
+    }
+}
