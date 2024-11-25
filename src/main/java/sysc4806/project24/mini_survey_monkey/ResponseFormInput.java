1e51934e7ecb709ed86be8bda09f0d251a426cc1
JackieSL1 <jackie@smallcity.ca>
2024-11-24 19:59:05 -0500

    Add multiple comment question responses

--- src/main/java/sysc4806/project24/mini_survey_monkey/ResponseFormInput.java
+++ src/main/java/sysc4806/project24/mini_survey_monkey/ResponseFormInput.java
@@ -2,9 +2,13 @@ package sysc4806.project24.mini_survey_monkey;
 
 import sysc4806.project24.mini_survey_monkey.models.Question;
 
-public final class ResponseFormData {
+public final class ResponseFormInput {
     private Question question;
     private String responseText;
+    private String questionType;
+
+    public ResponseFormInput() {
+    }
 
     public Question getQuestion() {
         return question;
@@ -20,6 +24,14 @@ public final class ResponseFormData {
 
     public void setResponseText(String responseText) {
         this.responseText = responseText;
+    }
+
+    public String getQuestionType() {
+        return questionType;
+    }
+
+    public void setQuestionType(String questionType) {
+        this.questionType = questionType;
     }
 };
 
