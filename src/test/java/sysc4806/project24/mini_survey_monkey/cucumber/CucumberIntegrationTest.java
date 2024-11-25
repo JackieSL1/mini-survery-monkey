8e623b5ed9c7a9a27321cd22122cee076c771025
Braeden Kloke <braedenkloke@gmail.com>
2024-11-25 12:24:37 -0500

    init cucumber integration tests

---
+++ src/test/java/sysc4806/project24/mini_survey_monkey/cucumber/CucumberIntegrationTest.java
@@ -0,0 +1,10 @@
+package sysc4806.project24.mini_survey_monkey.cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+public class CucumberIntegrationTest {
+}
---
+++ src/test/java/sysc4806/project24/mini_survey_monkey/cucumber/SpringIntegrationTest.java
@@ -0,0 +1,9 @@
+package sysc4806.project24.mini_survey_monkey.cucumber;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class SpringIntegrationTest {
+}
