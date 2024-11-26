0c4c83d7d79d82d25c037fdc2377e501c0892786
Anand Balaram <anandbalaram@cmail.carleton.ca>
2024-11-26 16:24:40 -0500

    Rearrange the file structure for BananaSteps and StoryRunner

--- src/test/java/sysc4806/project24/mini_survey_monkey/jbehave/StoryRunner.java
+++ src/test/java/sysc4806/project24/mini_survey_monkey/jbehave/StoryRunner.java
@@ -2,13 +2,12 @@ package sysc4806.project24.mini_survey_monkey.jbehave;
 
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
-import org.jbehave.core.junit.JUnitStories;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.junit.JUnitStories;
 import org.jbehave.core.junit.JUnitStory;
 import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.core.steps.CandidateSteps;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 import org.junit.runner.RunWith;
@@ -17,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import sysc4806.project24.mini_survey_monkey.jbehave.steps.BananaSteps;
 
 import java.util.List;
 
@@ -26,7 +26,7 @@ import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
 @SpringBootTest
 @AutoConfigureMockMvc
 //@SpringApplicationConfiguration(classes = { MiniSurveyMonkeyApplication.class})
-public class BananaStoryRunner extends JUnitStory {
+public class StoryRunner extends JUnitStories {
 
     @Autowired
     public MockMvc mvc;
--- src/test/java/sysc4806/project24/mini_survey_monkey/jbehave/steps/BananaSteps.java
+++ src/test/java/sysc4806/project24/mini_survey_monkey/jbehave/steps/BananaSteps.java
@@ -1,26 +1,11 @@
-package sysc4806.project24.mini_survey_monkey.jbehave;
+package sysc4806.project24.mini_survey_monkey.jbehave.steps;
 
 import org.jbehave.core.annotations.*;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
-import sysc4806.project24.mini_survey_monkey.controllers.BananaController;
-import sysc4806.project24.mini_survey_monkey.interceptors.CollectInterceptor;
-import sysc4806.project24.mini_survey_monkey.interceptors.CreateInterceptor;
-import sysc4806.project24.mini_survey_monkey.interceptors.ResponseInterceptor;
-import sysc4806.project24.mini_survey_monkey.interceptors.SummaryInterceptor;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
